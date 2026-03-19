885aaf296c7edd2faf239ab54e3adeb18b580e11
Physics <3150105638@zju.edu.cn>
2026-03-19 14:29:47 +0800

    revert for cuda-only

--- verl/utils/kernel/linear_cross_entropy.py
+++ verl/utils/kernel/linear_cross_entropy.py
@@ -34,8 +34,6 @@ import typing
 import torch
 import torch.distributed as dist
 
-from verl.plugin.platform import get_platform
-
 
 class LinearCrossEntropy(torch.autograd.Function):
     @staticmethod
@@ -65,7 +63,7 @@ class LinearCrossEntropy(torch.autograd.Function):
 
         assert isinstance(temperature, float), f"temperature must be a float, but got {type(temperature)}"
         assert isinstance(reduction, str), f"reduction must be a str, but got {type(reduction)}"
-        with get_platform().nvtx_range("LinearCrossEntropy-forward"):
+        with torch.cuda.nvtx.range("LinearCrossEntropy-forward"):
             from . import kernels
 
             REDUCTION = kernels.get_entropy_reduction_enum_number(reduction.lower())
@@ -92,7 +90,7 @@ class LinearCrossEntropy(torch.autograd.Function):
     def backward(ctx, dlogprobs: torch.Tensor, dentropy: torch.Tensor) -> list[torch.Tensor]:
         from . import kernels
 
-        with get_platform().nvtx_range("LinearCrossEntropy-backward"):
+        with torch.cuda.nvtx.range("LinearCrossEntropy-backward"):
             (hidden, weight, labels, _maximum, _accumulate, _entropy_b) = ctx.saved_tensors
             REDUCTION = ctx.REDUCTION
             dist_process_group = ctx.dist_process_group
